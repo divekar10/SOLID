9724fd3d30873c1bf23de83b2efab65c3cfd885b
akash141 <akash@nimapinfotech.com>
2022-02-09 10:33:53 +0530

    Uncomplete code

---
+++ DIPUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using DIP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person owner = new Person()
+            {
+                FirstName = "Akash",
+                lastName = "Divekar",
+                PhoneNumber = "7875200959",
+                EmailAddres = "akash@nimapinfotech.com"
+            };
+
+            Chore chore = new Chore()
+            {
+                ChoreName = "Take out the trash",
+                Owner = owner
+            };
+
+            chore.PerformedWork(2);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+
+            Console.ReadLine();
+        }
+    }
+}
